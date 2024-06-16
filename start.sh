faaa7c696207ebdc4dddc37f8c9faf23b701de4e
Arman <armhakobyan1997@mail.ru>
2024-06-17 00:49:03 +0300

    add run file

---
+++ start.sh
@@ -0,0 +1,22 @@
+# Установка pip, если не установлен (на Windows)
+python -m ensurepip --upgrade
+
+# Установка pip, если не установлен (на macOS/Linux)
+# python3 -m ensurepip --upgrade
+
+# Создание виртуального окружения
+# На Windows
+python -m venv venv --prompt "venv" --python=python3.10
+
+# На macOS/Linux
+# python3 -m venv venv --prompt "venv" --python=python3.10
+
+# Активация виртуального окружения
+# На Windows
+venv\Scripts\activate
+
+# На macOS/Linux
+# source venv/bin/activate
+
+# Установка зависимостей
+pip install -r ./metro_assist/requirements.txt
